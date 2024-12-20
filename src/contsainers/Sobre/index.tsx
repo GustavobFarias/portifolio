a50f06ff14bf3d746cec386e958ed08ac0d4c98c
Gustavo <gustavoburgerf@gmail.com>
2024-12-20 10:48:19 -0300

    criando a seção de projetos e o componente de projeto

--- src/contsainers/Sobre/index.tsx
+++ src/contsainers/Sobre/index.tsx
@@ -1,5 +1,6 @@
 import Titulo from "../../components/Titulo"
 import Paragrafo from "../../components/paragrafo"
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
@@ -7,6 +8,10 @@ const Sobre = () => (
     <Paragrafo>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quia reprehenderit corporis, inventore fugiat dolore deleniti sit accusantium! Autem libero dolorum sequi, qui ex in? Nihil totam similique nemo excepturi.
     </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=GustavobFarias&theme=radical&hide_border=true&include_all_commits=false&count_private=false" alt="" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=GustavobFarias&theme=radical&hide_border=true&include_all_commits=false&count_private=false&layout=compact" alt="" />
+    </GithubSecao>
   </section>
 )
 
---
+++ src/contsainers/Sobre/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const GithubSecao = styled.div`
+  display: flex;
+  margin-top: 32px;
+  margin-bottom: 64px;
+
+  @media (max-width: 768px) {
+    display: block;
+  }
+
+  img {
+    height: 200px;
+  }
+`
