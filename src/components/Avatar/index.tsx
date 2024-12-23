a569083428988986ff71b265c8ee912955096ca6
Gustavo <gustavoburgerf@gmail.com>
2024-12-23 10:34:54 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,6 @@
+import { FotoPerfil } from './styles'
+
+    <img src="" />
+const Avatar = () => <FotoPerfil src="https://github.com/GustavobFarias.png" alt="" />
+
+export default Avatar
--- src/contsainers/Sidebar/index.tsx
+++ src/contsainers/Sidebar/index.tsx
@@ -1,9 +1,20 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/paragrafo'
 import Titulo from '../../components/Titulo/index'
+
+import { Descricao } from './styles'
+import { BotaoTema } from './styles'
+import { SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/GustavobFarias.png" alt="" />
-    <Titulo fontSize={20}>Gustavo Farias</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Gustavo Farias</Titulo>
+      <Paragrafo tipo='secundario' fontSize={16}>GustavobFarias</Paragrafo>
+      <Descricao tipo='principal' fontSize={12}>Engenheiro Front-end</Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
